04590544b4315656fb59c2365415e7c57dff6387
Aung Kyaw Zorr <aungkyawzorr@Aungs-MacBook-Air.local>
2024-05-25 23:13:28 +0700

    Added membership form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import PdfFormFiller from './write_pdf'
+import PdfFormMembership from './write_membership_certificate'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <PdfFormFiller />
+     <PdfFormMembership />
     </>
   )
 }
